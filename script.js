b7d75c889720a8fa6d858bc2c87a88c59894cff4
Youchen Ren <youchen.r@gmail.com>
2019-04-08 22:46:25 -0400

    done

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+let color1 = document.querySelector(".color1");
+color1.setAttribute("value", "#005aa7")
+
+let color2 = document.querySelector(".color2");
+color2.setAttribute("value", "#fffde4")
+
+
+let body = document.getElementById("gradient");
+let h3 = document.querySelector("h3");
 
 function setGradient() {
 	body.style.background = 
-	"linear-gradient(to right, " 
+	"linear-gradient(to bottom, "
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
 	+ ")";
 
-	css.textContent = body.style.background + ";";
+	h3.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+setGradient();
 
+color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+
+
+let randomButton = document.getElementById("random");
+
+function getRandomizedRGBHex() {
+	let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+	while (color.length < 7){
+		color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	}
+	return color;
+}
+
+randomButton.addEventListener("click", () => {
+	color1.setAttribute("value", getRandomizedRGBHex());
+	color2.setAttribute("value", getRandomizedRGBHex());
+
+	setGradient();
+});
